758615d62ec8ad614b3c19d174b05aba25f4e55a
ghanshyam <ghanshyam.sikri@gmail.com>
2023-05-15 17:43:51 +0530

    missing file

---
+++ Test_driven_development/test/first_test.spec.js
@@ -0,0 +1,8 @@
+var assert = require('assert');
+describe('Array', function () {
+  describe('#indexOf()', function () {
+    it('running from the test class', function () {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+});
